9f92163cd06a3102f1386ab334f550b65afdfc40
EC2 Default User <ec2-user@ip-172-31-8-215.us-west-2.compute.internal>
2020-07-12 03:34:55 +0000

    step 3

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, BSpan } from 'bootstrap-4-react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Auth, Hub, Logger } from 'aws-amplify';
+// import { SignOut } from 'aws-amplify-react';
+import { JSignOut } from './auth';
 
 const HomeItems = props => (
   <React.Fragment>
@@ -26,8 +29,37 @@ const LoginItems = props => (
   </React.Fragment>
 )
 
+const logger = new Logger('Navigator');
+
 export default class Navigator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.loadUser = this.loadUser.bind(this);
+
+    Hub.listen('auth', this, 'navigator'); // Add this component as a listener of auth events.
+
+    this.state = { user: null }
+  }
+
+  componentDidMount() {
+    this.loadUser(); // The first check
+  }
+
+  onHubCapsule(capsule) {
+    logger.info('on Auth event', capsule);
+    this.loadUser(); // Triggered every time user sign in / out.
+  }
+
+  loadUser() {
+    Auth.currentAuthenticatedUser()
+      .then(user => this.setState({ user: user }))
+      .catch(err => this.setState({ user: null }));
+  }
+
   render() {
+    const { user } = this.state;
+
     return (
       <Navbar expand="md" dark bg="dark" fixed="top">
         <Navbar.Brand href="#">Journal</Navbar.Brand>
@@ -42,7 +74,10 @@ export default class Navigator extends Component {
               </Switch>
             </HashRouter>
           </Navbar.Nav>
-          <Navbar.Text>Greetings</Navbar.Text>
+          <Navbar.Text mr="2">
+            { user? 'Hi ' + user.username : 'Please sign in' }
+          </Navbar.Text>
+          { user && <JSignOut /> }
         </Navbar.Collapse>
       </Navbar>
     )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { Lead, BSpan } from 'bootstrap-4-react';
+import { Authenticator } from 'aws-amplify-react';
 
-export default class Home extends Component {
+import {
+  JSignIn,
+  JConfirmSignIn,
+  JSignUp,
+  JConfirmSignUp,
+  JForgotPassword,
+  JForgotPasswordReset
+} from '../components/auth';
+
+const CustomAuthenticator = props => (
+  <Authenticator hideDefault>
+    <JSignIn />
+    <JConfirmSignIn />
+    <JSignUp />
+    <JConfirmSignUp />
+    <JForgotPassword />
+    <JForgotPasswordReset />
+  </Authenticator>
+)
+
+export default class Login extends Component {
   render() {
+    const { user } = this.props;
+
     return (
-      <h1>Login</h1>
+      <React.Fragment>
+        { !user && <CustomAuthenticator /> }
+        { user && <Lead>You are signed in as <BSpan font="italic">{user.username}</BSpan>.</Lead> }
+      </React.Fragment>
     )
   }
 }
